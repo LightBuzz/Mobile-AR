3a2999a7db43fd3c60e36800f73b55c15d8f2e57
Konstantinos Egkarchos <konsnosl@gmail.com>
2019-06-05 12:40:35 +0300

    First commit

---
+++ Mobile-AR-Demo/Assets/Scripts/TapToPlace.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+/// <summary>
+/// Places an object on a tracked surface.
+/// </summary>
+public class TapToPlace : MonoBehaviour
+{
+    private ARRaycastManager raycastManager;
+
+    [SerializeField] private GameObject prefabToPlace;
+
+    void Start()
+    {
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                List<ARRaycastHit> hits = new List<ARRaycastHit>();
+                if (raycastManager.Raycast(touch.position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes))
+                {
+                    Pose pose = hits[0].pose;
+                    Quaternion rotation = pose.rotation;
+                    rotation *= Quaternion.Euler(Vector3.up * 180f); // look back.
+                    Instantiate(prefabToPlace, pose.position, rotation);
+
+                    Destroy(gameObject); // Don't spawn anything else
+                }
+                else
+                {
+                    Debug.Log("No surface found");
+                }
+            }
+        }
+    }
+}
---
+++ Mobile-AR-Demo/Assets/Scripts/TouchToRotate.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Rotates an object.
+/// </summary>
+public class TouchToRotate : MonoBehaviour
+{
+    private const string rotatingObjectTag = "Rotating Object";
+
+    GameObject rotatingObject;
+    Vector3 initialRotation;
+    Vector2 initialTouchLocation;
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    FindObjectTouched(touch);
+                    break;
+                case TouchPhase.Moved:
+                    RotateObject(touch);
+                    break;
+                case TouchPhase.Stationary:
+                    break;
+                case TouchPhase.Ended:
+                    ResetSelection();
+                    break;
+                case TouchPhase.Canceled:
+                    ResetSelection();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void ResetSelection()
+    {
+        rotatingObject = null;
+    }
+
+    private void FindObjectTouched(Touch touch)
+    {
+        ResetSelection();
+
+        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.yellow, 2f);
+
+        RaycastHit raycastHit;
+        if (Physics.Raycast(ray, out raycastHit))
+        {
+            if (raycastHit.collider != null && raycastHit.transform.tag == rotatingObjectTag)
+            {
+                // save starting values to calculate rotation
+                rotatingObject = raycastHit.transform.gameObject;
+                initialRotation = rotatingObject.transform.localRotation.eulerAngles;
+                initialTouchLocation = touch.position;
+            }
+            else
+            {
+                Debug.Log("Object isn't rotatable");
+            }
+        }
+        else
+        {
+            Debug.Log("No hit");
+        }
+    }
+
+    private void RotateObject(Touch touch)
+    {
+        if(rotatingObject != null)
+        {
+            float differenceX = initialTouchLocation.x - touch.position.x;
+            rotatingObject.transform.rotation = Quaternion.Euler(initialRotation + (Vector3.up * differenceX));
+        }
+    }
+}

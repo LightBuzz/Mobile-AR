f51f72e3528bcb8aee3aaae4554d5e7611bc9d9e
Konstantinos Egkarchos <konsnosl@gmail.com>
2020-06-12 15:58:38 +0300

    Updated project for Unity 2019.4

---
+++ Mobile-AR-Demo/Assets/App/Scripts/SceneChanger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(string nextSceneStr)
+    {
+        SceneManager.LoadScene(nextSceneStr);
+    }
+}
